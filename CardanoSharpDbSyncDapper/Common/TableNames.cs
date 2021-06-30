5ecd2b4b32b358e92365a5c6d6e59dc9b7b8aadd
Daniel-SchaeferJ <danieljschaefer@outlook.com>
2021-06-30 08:29:29 -0700

    Finished table mappings and made byte extension utility class with method

--- CardanoSharpDbSyncDapper/Common/TableNames.cs
+++ CardanoSharpDbSyncDapper/Common/TableNames.cs
@@ -8,35 +8,35 @@ namespace CardanoSharpDbSyncDapper.Common
 {
     public static class TableNames
     {
-        public const string Block = "Block";
-        public const string Delegation = "public.delegation";
-        public const string Epoch = "public.epoch";
-        public const string EpochParam = "public.epoch_param";
-        public const string EpochStake = "public.epoch_stake";
-        public const string MaTxMint = "public.ma_tx_mint";
-        public const string MaTxOut = "public.ma_tx_out";
-        public const string Metum = "public.meta";
-        public const string ParamProposal = "public.param_proposal";
-        public const string PoolHash = "public.pool_hash";
-        public const string PoolMetaDatum = "public.pool_meta_data";
-        public const string PoolOwner = "public.pool_owner";
-        public const string PoolRelay = "public.pool_relay";
-        public const string PoolRetire = "public.pool_retire";
-        public const string PoolUpdate = "public.pool_update";
-        public const string Reserve = "public.reserve";
-        public const string Reward = "public.reward";
-        public const string SchemaVersion = "public.schema_version";
-        public const string SlotLeader = "public.slot_leader";
-        public const string StakeAddress = "public.stake_address";
-        public const string StakeDeregistration = "public.stake_deregistration";
-        public const string StakeRegistration = "public.stake_registration";
-        public const string Treasury = "public.treasury";
-        public const string Tx = "public.tx";
-        public const string TxIn = "public.tx_in";
-        public const string TxMetadatum = "public.tx_metadata";
-        public const string TxOut = "public.tx_out";
-        public const string UtxoView = "public.utxo_view";
-        public const string Withdrawal = "public.withdrawal";
-        public const string OrphanedReward = "OrphanedReward";
+        public const string Block = "block";
+        public const string Delegation = "delegation";
+        public const string Epoch = "epoch";
+        public const string EpochParam = "epoch_param";
+        public const string EpochStake = "epoch_stake";
+        public const string MaTxMint = "ma_tx_mint";
+        public const string MaTxOut = "ma_tx_out";
+        public const string Metum = "meta";
+        public const string ParamProposal = "param_proposal";
+        public const string PoolHash = "pool_hash";
+        public const string PoolMetaDatum = "pool_meta_data";
+        public const string PoolOwner = "pool_owner";
+        public const string PoolRelay = "pool_relay";
+        public const string PoolRetire = "pool_retire";
+        public const string PoolUpdate = "pool_update";
+        public const string Reserve = "reserve";
+        public const string Reward = "reward";
+        public const string SchemaVersion = "schema_version";
+        public const string SlotLeader = "slot_leader";
+        public const string StakeAddress = "stake_address";
+        public const string StakeDeregistration = "stake_deregistration";
+        public const string StakeRegistration = "stake_registration";
+        public const string Treasury = "treasury";
+        public const string Tx = "tx";
+        public const string TxIn = "tx_in";
+        public const string TxMetadatum = "tx_metadata";
+        public const string TxOut = "tx_out";
+        public const string UtxoView = "utxo_view";
+        public const string Withdrawal = "withdrawal";
+        public const string OrphanedReward = "orphaned_reward";
     }
 }
---
+++ CardanoSharpDbSyncDapper/Extensions/ByteExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardanoSharpDbSyncDapper.Extensions
+{
+    public static class ByteExtension
+    {
+        public static string GetHex(this byte[] ba)
+        {
+            return BitConverter.ToString(ba).Replace("-", "").ToLower();
+
+        }
+    }
+}
