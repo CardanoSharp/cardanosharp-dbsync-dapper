a5059b3f2b9a2ddfe47cf27755920907efaa3b7f
Daniel-SchaeferJ <danieljschaefer@outlook.com>
2021-07-02 09:10:42 -0700

    Added mappings

---
+++ CardanoSharpDbSyncDapper/Mappings/PoolOwnerMap.cs
@@ -0,0 +1,16 @@
+﻿using CardanoSharpDbSyncDapper.Models;
+using Dapper.FluentMap.Mapping;
+
+#nullable disable
+
+namespace CardanoSharpDbSyncDapper.Mappings
+{
+    public class PoolOwnerMap : EntityMap<PoolOwner>
+    {
+        public PoolOwnerMap()
+        {
+            Map(p => p.PoolHashId).ToColumn("pool_hash_id");
+            Map(p => p.RegisteredTxId).ToColumn("registered_tx_id");
+        }
+    }
+}
