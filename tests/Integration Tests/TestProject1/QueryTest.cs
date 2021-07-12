76e86a5d0887b216ce799149e306a6cc0a5852d2
Daniel Schaefer <danieljschaefer@outlook.com>
2021-07-11 20:27:22 -0700

    Beginning of tests
    Begin work on the integration tests

---
+++ tests/Integration Tests/TestProject1/QueryTest.cs
@@ -0,0 +1,35 @@
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestProject1
+{
+    public class QueryTest
+    {
+        private readonly string connectionString = "Data Source=localhost; Initial Catalog = cardano; User ID = sa; Password=!Password";
+        protected IDbConnection GetConnection => new SqlConnection(connectionString);
+        [Fact]
+        public void GetBlockNumberAsync()
+        {
+            string _tableName = "block"; 
+            using (var conn = GetConnection)
+            {
+                try
+                {
+                    conn.Open();
+                    var result = conn.Query(
+                        $"select * from {_tableName} where block_number = '2'",
+                        new
+                        {
+                            TableName = _tableName,
+                        });
+
+                    Assert.Single(result.AsList());
+                }
+            }
+
+        }
+    }
+}
